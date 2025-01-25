bbfb5e52b37d4aca2a0460794231f2eecc388585
gwang-coding <jaegwang1238@gmail.com>
2025-01-26 01:32:21 +0900

    Mysql 연동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 // import Image from 'next/image';
+import MysqlProduct from '@/components/MysqlProduct';
 import ProductSearch from '@/components/ProductSearch';
 
 export default function Home() {
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <ProductSearch />
+            <div>mysql data</div>
+            <MysqlProduct />
         </div>
     );
 }
