9461aee9cdc305dc089a8893c6211c31374a5529
gwang-coding <jaegwang1238@gmail.com>
2025-02-05 06:23:43 +0900

    refactor: BuyButton component to display total price

--- src/container/BuyButton.tsx
+++ src/container/BuyButton.tsx
@@ -6,9 +6,11 @@ import { useRouter } from 'next/navigation';
 interface ButtonProps {
     onClick?: () => void;
     navigateTo?: string;
+    totalProduct: number;
+    getTotalPrice: number;
 }
 
-function BuyButton({ onClick, navigateTo }: ButtonProps) {
+function BuyButton({ getTotalPrice, totalProduct, onClick, navigateTo }: ButtonProps) {
     const router = useRouter();
 
     const handleClick = () => {
@@ -17,9 +19,14 @@ function BuyButton({ onClick, navigateTo }: ButtonProps) {
     };
 
     return (
-        <button onClick={handleClick} className="py-4 font-semibold w-full text-white bg-[#4285f4]">
-            총한개의 상품 구매하기
-        </button>
+        <div className="sticky bottom-0">
+            <div className="border-t bg-white border-gray-300 py-1 font-bold text-lg text-center">
+                <p>총 {getTotalPrice}원</p>
+            </div>
+            <button onClick={handleClick} className="py-4 font-bold w-full text-xl text-white bg-[#4285f4]">
+                총{totalProduct}개의 상품 구매하기
+            </button>
+        </div>
     );
 }
 
