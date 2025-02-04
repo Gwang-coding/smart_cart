72a9b8cc135f37c024739ddf482efd1dd45522ed
gwang-coding <jaegwang1238@gmail.com>
2025-02-05 06:24:47 +0900

    improve: add handler

--- src/container/BuyTop.tsx
+++ src/container/BuyTop.tsx
@@ -4,7 +4,13 @@
 import React, { useState } from 'react';
 import Modal from '@/container/BuyModal';
 
-export default function BuyTop() {
+interface SelectProps {
+    removeSelected: () => void;
+    selectAll: boolean;
+    handleSelectAll: () => void;
+}
+
+export default function BuyTop({ handleSelectAll, selectAll, removeSelected }: SelectProps) {
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     // 모달을 열기 위한 핸들러
@@ -13,9 +19,9 @@ export default function BuyTop() {
     };
 
     // 삭제 처리 핸들러
-    const handleConfirmDelete = () => {
-        alert('상품이 삭제되었습니다.');
+    const handleCloseAndDelete = () => {
         setIsModalOpen(false); // 모달 닫기
+        removeSelected();
     };
 
     // 모달을 닫기 위한 핸들러
@@ -24,22 +30,27 @@ export default function BuyTop() {
     };
 
     return (
-        <div className="w-full flex justify-between items-center py-3 px-4 border-b-[1px]  border-gray-300">
-            <div className="flex items-center">
-                <input type="checkbox" className="accent-[#4285f4] w-7 h-7 mr-2 " />
-                <label htmlFor="selectAll" className="text-lg font-medium">
-                    전체 선택
-                </label>
+        <div className=" sticky top-0 bg-white z-10 ">
+            <div className="p-2 text-center">
+                <p className="text-xl font-bold">장바구니</p>
             </div>
-            <button onClick={handleDelete} className="px-3 py-1 text-black font-semibold ">
-                선택 삭제
-            </button>
+            <div className="w-full flex justify-between items-center py-2 px-4 border-b-[1px]  border-gray-300">
+                <div className="flex items-center">
+                    <input checked={selectAll} onChange={handleSelectAll} type="checkbox" className="accent-[#4285f4] w-7 h-7 mr-2 " />
+                    <label htmlFor="selectAll" className="text-base font-bold">
+                        전체 선택
+                    </label>
+                </div>
+                <button onClick={handleDelete} className="px-3 py-1 text-sm text-gray-600 font-semibold ">
+                    선택 삭제
+                </button>
 
-            <Modal
-                isOpen={isModalOpen} // 모달의 열림 상태를 전달
-                handleClose={handleCloseModal} // 모달 닫기 핸들러 전달
-                handleConfirm={handleConfirmDelete} // 삭제 확인 핸들러 전달
-            />
+                <Modal
+                    isOpen={isModalOpen} // 모달의 열림 상태를 전달
+                    handleClose={handleCloseModal} // 모달 닫기 핸들러 전달
+                    handleConfirm={handleCloseAndDelete} // 삭제 확인 핸들러 전달
+                />
+            </div>
         </div>
     );
 }
