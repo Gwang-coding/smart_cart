53dcdf14c5262f405368f2d087a8b2be9b947df7
gwang-coding <jaegwang1238@gmail.com>
2025-04-03 22:19:25 +0900

    Refactory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,45 @@
-// import Image from 'next/image';
-import MysqlProduct from '@/components/MysqlProduct';
-import ProductSearch from '@/components/ProductSearch';
-
+'use client';
+import { useState } from 'react';
+import Top from '@/components/Top';
 import ShoppingCart from '@/container/ShoppingCart';
-
+import SearchProduct from '@/container/SearchProduct';
+import CheckInventory from '@/container/CheckInventory';
+import QR from '@/container/QR';
 export default function Home() {
-    const handleClick = () => {
-        alert('버튼이 클릭되었습니다!');
+    const [activeComponent, setActiveComponent] = useState('ShoppingCart');
+
+    // Navigation handler that will be passed to Top component
+    const handleNavigation = (componentName: string) => {
+        setActiveComponent(componentName);
     };
+
     return (
         <div className="flex flex-col h-screen">
-            <ShoppingCart />
-            {/* <ProductSearch /> */}
+            <div className="w-full h-full">
+                <Top text={getComponentTitle(activeComponent)} />
 
-            {/* <MysqlProduct /> */}
-
-            <div></div>
+                {/* Render the active component based on state */}
+                {activeComponent === 'ShoppingCart' && <ShoppingCart />}
+                {activeComponent === 'CheckInventory' && <CheckInventory />}
+                {activeComponent === 'QR' && <QR />}
+                {activeComponent === 'SearchProduct' && <SearchProduct />}
+            </div>
         </div>
     );
+}
+
+// Helper function to get the title for the Top component
+function getComponentTitle(componentName: string): string {
+    switch (componentName) {
+        case 'ShoppingCart':
+            return '장바구니';
+        case 'CheckInventory':
+            return '재고확인';
+        case 'SearchProduct':
+            return '물품 위치찾기';
+        case 'QR':
+            return 'QR';
+        default:
+            return '장바구니';
+    }
 }
