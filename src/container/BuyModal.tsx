fc80e619ff416b91a5d371bc343430c74e384d05
gwang-coding <jaegwang1238@gmail.com>
2025-04-03 22:23:02 +0900

    Refactory

--- src/container/BuyModal.tsx
+++ src/container/BuyModal.tsx
@@ -7,15 +7,16 @@ interface ModalProps {
     isOpen: boolean; // 모달이 열렸는지 여부
     handleConfirm: () => void; // 삭제 확인 버튼을 눌렀을 때의 핸들러
     handleClose: () => void; // 모달 닫기 버튼을 눌렀을 때의 핸들러
+    content: string;
 }
 
-function Modal({ isOpen, handleConfirm, handleClose }: ModalProps) {
+function Modal({ content, isOpen, handleConfirm, handleClose }: ModalProps) {
     if (!isOpen) return null; // isOpen이 false일 때는 모달을 렌더링하지 않음
 
     return (
         <div className="fixed inset-0 z-50 flex justify-center items-center bg-gray-500 bg-opacity-50">
             <div className="bg-white p-6 rounded-lg shadow-lg w-90">
-                <h2 className="text-lg font-semibold mb-4">선택한 상품을 삭제하시겠습니까?</h2>
+                <h2 className="text-lg font-semibold mb-4">{content}</h2>
                 <div className="flex justify-between space-x-4">
                     <button onClick={handleClose} className="px-10 py-2 border-2 border-gray-200 text-black font-bold rounded-md ">
                         취소
