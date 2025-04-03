f9a3dca82c7b3600d1e03cb6110a75967b0e1eab
gwang-coding <jaegwang1238@gmail.com>
2025-04-03 22:22:52 +0900

    Refactory

--- src/container/BuyButton.tsx
+++ src/container/BuyButton.tsx
@@ -4,26 +4,21 @@
 import { useRouter } from 'next/navigation';
 
 interface ButtonProps {
-    onClick?: () => void;
+    Click?: () => void;
     navigateTo?: string;
     totalProduct: number;
     getTotalPrice: number;
 }
 
-function BuyButton({ getTotalPrice, totalProduct, onClick, navigateTo }: ButtonProps) {
+function BuyButton({ getTotalPrice, totalProduct, Click, navigateTo }: ButtonProps) {
     const router = useRouter();
-
-    const handleClick = () => {
-        alert('결제 ㄱㄱ');
-        // if (navigateTo) router.push(navigateTo); // 페이지 이동
-    };
 
     return (
         <div className="sticky bottom-0">
             <div className="border-t bg-white border-gray-300 py-1 font-bold text-lg text-center">
                 <p>총 {getTotalPrice}원</p>
             </div>
-            <button onClick={handleClick} className="py-4 font-bold w-full text-xl text-white bg-[#4285f4]">
+            <button onClick={Click} className="py-4 font-bold w-full text-xl text-white bg-[#4285f4]">
                 총{totalProduct}개의 상품 구매하기
             </button>
         </div>
