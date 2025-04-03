bffcee02133ea586cc569ce539f3c13372ed9c4c
gwang-coding <jaegwang1238@gmail.com>
2025-04-03 22:26:28 +0900

    Feat: get gridpoint on searchproduct page

---
+++ src/container/SearchProduct.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import GridPoints from '@/components/GridPoints';
+
+function SearchProduct() {
+    return (
+        <div className="p-8">
+            <GridPoints width={300} height={400} gridSize={15} imageSrc="../images/martmap.png" />
+        </div>
+    );
+}
+
+export default SearchProduct;
