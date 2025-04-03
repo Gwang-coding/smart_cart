4d7ae0cc8cae65a05cfe32c64d9515b93dd86757
gwang-coding <jaegwang1238@gmail.com>
2025-04-03 22:20:21 +0900

    Feat: [id] QR page

---
+++ src/app/[id]/QR/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import QR from '@/container/QR';
+
+export default function Page() {
+    return (
+        <div className="flex flex-col h-screen">
+            <div className="w-full h-full ">
+                <QR />
+            </div>
+        </div>
+    );
+}
