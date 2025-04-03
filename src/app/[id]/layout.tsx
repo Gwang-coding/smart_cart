dcb623dc583b48a6a13713fb78390f99c5bf7f4b
gwang-coding <jaegwang1238@gmail.com>
2025-04-03 22:19:38 +0900

    Feat: get Top components

---
+++ src/app/[id]/layout.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { use } from 'react';
+import { usePathname, useSearchParams } from 'next/navigation';
+import Top from '@/components/Top';
+
+interface LayoutProps {
+    children: React.ReactNode;
+    params: Promise<{ id: string }>;
+}
+
+export default function Layout({ children, params }: LayoutProps) {
+    const unwrappedParams = use(params);
+    const id = unwrappedParams.id;
+    const searchParams = useSearchParams();
+    const menuText = searchParams?.get('menu') || '장바구니'; // 기본값은 '장바구니'
+
+    return (
+        <div className="flex flex-col h-screen">
+            <div className="w-full h-full">
+                <Top text={menuText} cartid={id} />
+                {children}
+            </div>
+        </div>
+    );
+}
