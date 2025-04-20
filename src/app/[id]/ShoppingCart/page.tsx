5e536cd7297c354ec3a991174430317220019d9b
gwang-coding <jaegwang1238@gmail.com>
2025-04-20 16:43:54 +0900

    refactory

--- src/app/[id]/ShoppingCart/page.tsx
+++ src/app/[id]/ShoppingCart/page.tsx
@@ -2,11 +2,19 @@
 
 import ShoppingCart from '@/container/ShoppingCart';
 
+import { useParams, useSearchParams } from 'next/navigation';
 export default function Page() {
+    const params = useParams(); // useParams로 동적 라우트 파라미터 접근
+    const searchParams = useSearchParams(); // 쿼리 파라미터 접근
+
+    // params.id가 배열일 경우 첫 번째 값만 사용하고, 없으면 '0'을 사용하여 parseInt
+    const cartId = parseInt(Array.isArray(params?.id) ? params.id[0] : params?.id ?? '0');
+    const token = searchParams?.get('token') ?? '';
+
     return (
         <div className="flex flex-col h-screen">
             <div className="w-full h-full ">
-                <ShoppingCart />
+                <ShoppingCart cartId={cartId} token={token} />
             </div>
         </div>
     );
