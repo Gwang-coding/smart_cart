235e79ceb7e23b97856850828ec3e16912c03fed
gwang-coding <jaegwang1238@gmail.com>
2025-04-20 16:51:17 +0900

    refactory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,41 @@
 import Top from '@/components/Top';
 import ShoppingCart from '@/container/ShoppingCart';
-import { useSearchParams } from 'next/navigation';
 
 interface CartPageProps {
     params: {
         id: string;
     };
+    searchParams: {
+        token: string;
+    };
 }
 
-export default function Home({ params }: CartPageProps) {
+export default function Home({ params, searchParams }: CartPageProps) {
     const cartId = parseInt(params.id);
-    const searchParams = useSearchParams(); // Next.js 훅
-    const token = searchParams?.get('token') ?? ''; // get으로 불러와야 함
-
+    const { token } = searchParams as { token: string };
     return (
         <div className="flex flex-col h-screen">
             <div className="w-full h-full">
                 <Top />
+
                 <ShoppingCart cartId={cartId} token={token} />
             </div>
         </div>
     );
 }
+
+// Helper function to get the title for the Top component
+// function getComponentTitle(componentName: string): string {
+//     switch (componentName) {
+//         case 'ShoppingCart':
+//             return '장바구니';
+//         case 'CheckInventory':
+//             return '재고확인';
+//         case 'SearchProduct':
+//             return '물품 위치찾기';
+//         case 'QR':
+//             return 'QR';
+//         default:
+//             return '장바구니';
+//     }
+// }
