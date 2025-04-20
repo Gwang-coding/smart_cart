7754becbc9d35906f6dbe0b0867fd2b6383a3597
gwang-coding <jaegwang1238@gmail.com>
2025-04-20 16:20:22 +0900

    Delete: use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,15 @@
-'use client';
-
+// 'use client' 제거!
 import Top from '@/components/Top';
 import ShoppingCart from '@/container/ShoppingCart';
-import { useSearchParams } from 'next/navigation';
 
 interface CartPageProps {
-    params: {
-        id: string;
-    };
+    params: { id: string };
+    searchParams: { token?: string };
 }
 
-export default function Home({ params }: CartPageProps) {
+export default function Home({ params, searchParams }: CartPageProps) {
     const cartId = parseInt(params.id);
-    const searchParams = useSearchParams(); // Next.js 훅
-    const token = searchParams?.get('token') ?? ''; // get으로 불러와야 함
+    const token = searchParams?.token ?? '';
 
     return (
         <div className="flex flex-col h-screen">
