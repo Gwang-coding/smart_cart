f83c2a5bf84cccd039b4d5250468e828995542c1
gwang-coding <jaegwang1238@gmail.com>
2025-05-13 02:24:00 +0900

    Feat: main [id] layout for TopWrapper

---
+++ src/app/main/[id]/layout.tsx
@@ -0,0 +1,12 @@
+// app/main/[id]/layout.tsx
+import TopWrapper from './top-wrapper';
+
+export default async function Layout({ children, params }: { children: React.ReactNode; params: { id: string } }) {
+    const unwrappedParams = await params;
+    return (
+        <div className="flex flex-col h-screen">
+            <TopWrapper cartId={unwrappedParams.id} />
+            <main className="flex-grow overflow-auto">{children}</main>
+        </div>
+    );
+}
