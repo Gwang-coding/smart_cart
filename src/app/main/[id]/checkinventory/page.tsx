5821b7e75258a20afbb12b141d3976098223b13a
gwang-coding <jaegwang1238@gmail.com>
2025-05-15 17:46:27 +0900

    Refactory

--- src/app/main/[id]/checkinventory/page.tsx
+++ src/app/main/[id]/checkinventory/page.tsx
@@ -2,7 +2,7 @@
 import { Suspense } from 'react';
 import CheckInventoryPageClient from './client-page';
 
-export default async function ShoppingCartPage({ params }: { params: Promise<{ id: string }> }) {
+export default async function CheckInventoryPage({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params; // ✅ ② 실제로는 여기서 await
     return (
         <Suspense fallback={<div>로딩 중...</div>}>
