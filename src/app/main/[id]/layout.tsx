149a67715596ef158caeff58c6f8fdcd442d50a2
gwang-coding <jaegwang1238@gmail.com>
2025-05-15 17:46:59 +0900

    Refactory

--- src/app/main/[id]/layout.tsx
+++ src/app/main/[id]/layout.tsx
@@ -1,11 +1,10 @@
 // app/main/[id]/layout.tsx
 import TopWrapper from './top-wrapper';
 
-export default async function Layout({ children, params }: { children: React.ReactNode; params: Promise<{ id: string }> }) {
-    const unwrappedParams = await params;
+export default async function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex flex-col h-screen">
-            <TopWrapper cartId={unwrappedParams.id} />
+            <TopWrapper />
             <main className="flex-grow overflow-auto">{children}</main>
         </div>
     );
