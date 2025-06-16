d0aeef05ca3ed8c42eb4d376de456b45df9257a6
gwang-coding <jaegwang1238@gmail.com>
2025-06-16 20:30:29 +0900

    Refactory

--- src/app/main/[id]/top-wrapper.tsx
+++ src/app/main/[id]/top-wrapper.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
 import Top from '@/components/Top';
 import { useCart } from '@/contexts/CartContext';
 
@@ -9,14 +8,6 @@ export default function TopWrapper() {
     const searchParams = useSearchParams();
     const menuText = searchParams?.get('menu') || '장바구니';
     const { cartId, sessionToken } = useCart();
-
-    // 모든 Hooks는 조건문 이전에 호출되어야 함
-    useEffect(() => {
-        // cartId가 있을 때만 로깅
-        if (cartId) {
-            console.log('Current session token:', sessionToken);
-        }
-    }, [sessionToken, cartId]);
 
     // 조건부 렌더링은 Hooks 호출 이후에 처리
     if (!cartId) {
